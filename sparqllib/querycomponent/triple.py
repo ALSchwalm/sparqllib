11322545c431cb9b65a125d8f678717c53f062be
Adam Schwalm <adamschwalm@gmail.com>
2014-10-15 21:17:55 -0500

    Allow comparison between triple and tuple

--- sparqllib/querycomponent/triple.py
+++ sparqllib/querycomponent/triple.py
@@ -8,6 +8,15 @@ class Triple(QueryComponent):
         self.relationship = relationship
         self.object = object
 
+    # Allow a tuple to compare equal to a Triple
+    def __eq__(self, other):
+        if not isinstance(other, Triple):
+            other = Triple(*other)
+
+        return (other.subject == self.subject and
+                other.relationship == self.relationship and
+                other.object == self.object)
+
     def serialize(self):
         return "{subject} {relationship} {object} .\n".format(
             subject=serialize_rdf_term(self.subject),
