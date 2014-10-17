9571b83e9125ab4570409dd9186fc759a0e98bd0
Adam Schwalm <adamschwalm@gmail.com>
2014-10-16 20:09:53 -0500

    Add operator += for groups

--- sparqllib/querycomponent/group.py
+++ sparqllib/querycomponent/group.py
@@ -1,5 +1,5 @@
 from sparqllib.querycomponent import QueryComponent
-from sparqllib.utils import convert_components
+from sparqllib.utils import convert_component, convert_components
 
 class Group(QueryComponent):
     ''' Representation of a SPARQL group element
@@ -9,6 +9,10 @@ class Group(QueryComponent):
     '''
     def __init__(self, *components):
         self.components = convert_components(components)
+
+    def __iadd__(self, component):
+        self.components.append(convert_component(component))
+        return self
 
     def serialize(self):
         serialized = "{"
