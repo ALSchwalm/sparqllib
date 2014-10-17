c5ac7ef219081cbb7ed5d269033aa67224d42f40
Adam Schwalm <adamschwalm@gmail.com>
2014-10-16 20:08:20 -0500

    Remove usage of abstractmethod from QueryComponent

--- sparqllib/querycomponent/querycomponent.py
+++ sparqllib/querycomponent/querycomponent.py
@@ -1,8 +1,3 @@
-import abc
 
 class QueryComponent:
-    @abc.abstractmethod
-    def serialize(self):
-        ''' Should return a valid SPARQL string representation of the statement.
-        '''
-        pass
+    pass
