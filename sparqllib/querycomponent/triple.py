bb5f4e9ac098b24017db9dec9a79cc45e2ea628f
Adam Schwalm <adamschwalm@gmail.com>
2014-10-17 22:49:04 -0500

    Return false if Triple comparison would cause exception

--- sparqllib/querycomponent/triple.py
+++ sparqllib/querycomponent/triple.py
@@ -10,12 +10,15 @@ class Triple(QueryComponent):
 
     # Allow a tuple to compare equal to a Triple
     def __eq__(self, other):
-        if not isinstance(other, Triple):
-            other = Triple(*other)
+        try:
+            if not isinstance(other, Triple):
+                other = Triple(*other)
 
-        return (other.subject == self.subject and
-                other.relationship == self.relationship and
-                other.object == self.object)
+            return (other.subject == self.subject and
+                    other.relationship == self.relationship and
+                    other.object == self.object)
+        except TypeError:
+            return False
 
     def serialize(self):
         return "{subject} {relationship} {object} .\n".format(
